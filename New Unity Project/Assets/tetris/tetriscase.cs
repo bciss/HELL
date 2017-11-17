a32f2baae18831f8f7687c8fa402bac97158c17f
Gabriel FOURNIER <gabfou95@gmail.com>
2017-11-17 22:43:49 +0100

    fdsf

--- New Unity Project/Assets/tetris/tetriscase.cs
+++ New Unity Project/Assets/tetris/tetriscase.cs
@@ -9,7 +9,7 @@ public class tetriscase : MonoBehaviour {
 	public tetriscase left = null;
 
 	void Start () {
-		
+		Camera.main.GetComponent<controllertetris>().checkpoofneeded.AddListener(checkpoof);
 	}
 	
 	// Update is called once per frame
@@ -26,10 +26,55 @@ public class tetriscase : MonoBehaviour {
 	public void checkpoof()
 	{
 		RaycastHit2D righthit = Physics2D.Raycast(transform.position, new Vector2(1, 0));
-		right = righthit.collider.gameObject.GetComponent<tetriscase>();
+		if (righthit == false)
+			right = null;
+		else
+			right = righthit.collider.gameObject.GetComponent<tetriscase>();
 
 		RaycastHit2D lefthit = Physics2D.Raycast(transform.position, new Vector2(-1, 0));
-		left = lefthit.collider.gameObject.GetComponent<tetriscase>();
-		
+		if (lefthit == false)
+			left = null;
+		else
+			left = lefthit.collider.gameObject.GetComponent<tetriscase>();
+
+		if (right == this || left == this)
+		{
+			Debug.Log("dafuq346");
+			return ;
+		}
+
+		tetriscase tmp = this;
+		int nballign = 1;
+		while (tmp.left != null)
+		{
+			nballign++;
+			tmp = tmp.left;
+		}
+		tmp = this;
+		while (tmp.right != null)
+		{
+			nballign++;
+			tmp = tmp.right;
+		}
+	
+		if (nballign > 9)
+		{
+			tetriscase tmp2 = null;
+			tmp = this.left;
+			while (tmp != null)
+			{
+				tmp2 = tmp;
+				tmp = tmp.left;
+				GameObject.Destroy(tmp2.gameObject);
+			}
+			tmp = this.right;
+			while (tmp != null)
+			{
+				tmp2 = tmp;
+				tmp = tmp.right;
+				GameObject.Destroy(tmp2.gameObject);
+			}
+			GameObject.Destroy(this);
+		}
 	}
 }
