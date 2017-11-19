b2988ed6e1d3ae2c43cc3afc754cc76151ea67d3
Babacar CISS <bciss@e1r10p3.42.fr>
2017-11-19 04:30:51 +0100

    space pew pew

--- New Unity Project/Assets/Scripts/foe_controller.cs
+++ New Unity Project/Assets/Scripts/foe_controller.cs
@@ -5,44 +5,54 @@ using UnityEngine;
 public class foe_controller : MonoBehaviour {
 
 
-	public	GameObject	Ammo;
-	public	float		SpeedH;
-	public	float		SpeedV;
+	public	GameObject		Ammo;
+	public	GameObject		GameManager;
+	public	float			SpeedH;
+	public	float			SpeedV;
 
-	private	float		i;
-	private float		j;
-	private	GameObject	curShoot;
+	private	float			i;
+	private float			j;
+	private	float			vit;
+	private	GameObject		curShoot;
+	private	Game_Manager	GM;
 
 
 	// Use this for initialization
 	void Start () {
+		GM = GameManager.GetComponent<Game_Manager>();
 		i = 0;
+		vit = 10;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if ((Input.GetKeyDown("space") && !curShoot) || (Random.Range(0, 1000) <= 0))
-			Shoot();
-		if (i == 0) {
-			transform.position = new Vector3(transform.position.x, transform.position.y - SpeedV, transform.position.z);
-			j = SpeedH;
-		} else if (i == 25) {
-			transform.position = new Vector3(transform.position.x, transform.position.y - SpeedV, transform.position.z);
-			j = -SpeedH;
-		} else {
-			transform.position = new Vector3(transform.position.x + j, transform.position.y, transform.position.z);
+		if (GM.GameState) {
+			if ((Random.Range(0f, vit) <= 0.01f))
+				Shoot();
+			if (i == 0) {
+				transform.position = new Vector3(transform.position.x, transform.position.y - SpeedV * Time.deltaTime * 60f, 0);
+				j = SpeedH;
+			} else if (i == 25) {
+				transform.position = new Vector3(transform.position.x, transform.position.y - SpeedV * Time.deltaTime * 60f, 0);
+				j = -SpeedH;
+			} else {
+				transform.position = new Vector3(transform.position.x + j * Time.deltaTime * 60f, transform.position.y, 0);
+				vit *= 0.999f;
+			}
+			i += j;
 		}
-		i += j;
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		if (other.tag == "Player")
+		if (other.tag == "Player") {
+			GM.ScoreUp(20);
+			// GM.ScoreUp(20);
 			GameObject.Destroy(gameObject);
+		}
 	}
 	
 	void Shoot() {
 		curShoot = Instantiate(Ammo, transform.position, Quaternion.identity);
-		// Debug.Log("shoot !!!");
 	}
 }
