335b135b56f781dd8e10f04b9769de4a7df7e432
Gabriel FOURNIER <gabfou95@gmail.com>
2017-11-19 04:30:44 +0100

    fds

---
+++ New Unity Project/Assets/playerloadingdevilcontroller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerloadingdevilcontroller : MonoBehaviour {
+
+	public float xmin;
+	public float xmax;
+	public float ymin;
+	public float ymax;
+	public blblbl b;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float move =  Input.GetAxisRaw("Horizontal");
+		float move2 =  Input.GetAxisRaw("Vertical");
+
+		float newx = move * Time.deltaTime * 5 + transform.position.x;
+		float newy = move2 * Time.deltaTime * 5 + transform.position.y;
+		transform.position += new Vector3((newx > xmin && newx < xmax) ? move * Time.deltaTime * 5 : 0, (newy > ymin && newy < ymax) ? move2 * Time.deltaTime * 5 : 0, 0);
+		if (move == 0 && move2 == 0)
+			b.isactive = false;
+		else
+			b.isactive = true;
+	}
+
+	// void OnDrawGizmos()
+	// {
+	// 	Gizmos.color = Color.red;
+	// 	Gizmos.DrawWireCube;
+	// }
+}
